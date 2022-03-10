80bea16b17c5ae3aecf4b7a1478b2754ed2ba0e2
waitherakamau <effencekamau@gmail.com>
2022-03-10 08:57:16 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,25 @@
+fun main(){
+hobies()
+}k=divide(4%2)
+m=(para)
+word(matchbox)
+
+fun hobies(){
+    var name:String =intArrayOf( "dance","sing","football")
+    printlnarrayOf( int1:dance,int2:sing,int3:football)
+
+
+}
+fun divide(){
+    var divide:num(4%2)
+    println(num())
+
+}fun para(){
+    var parameter:String("hy"",+ my name is effence""+ and i am 19 years")
+    println(content:Tostring)
+
+
+}fun word(){
+    var String:("matchbox")
+    println(lenghth)
+}
